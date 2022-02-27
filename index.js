d4797f8b165f4cbcd6bffb7cde2f736ed9630212
Hallcoder <hallcoder25@gmail.com>
2022-02-27 14:31:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ let getTweetsfunc = document.getElementById("getTweets");
 let getUsersfunc = document.getElementById("getUsers");
 let getTweetDetailsfunc = document.getElementById("getTweetDetails");
 let getInfofunc = document.getElementById("getInfo");
-let getLinksfunc = document.getElementById("getLinks");
+let getLinksfunc = document.getElementById("getLinks")
 let idForm = document.getElementById("idFormInput");
 let screenName = document.getElementById("nameInput");
 
+
 getTweetsfunc.addEventListener("click", getTweets);
-function getTweets(e) {
-  fetch("https://tweet-favorites.herokuapp.com/tweets")
+ function getTweets(e) {
+   fetch("https://tweet-favorites.herokuapp.com/tweets")
     .then((res) => res.json())
     .then((data) => {
       let response = document.getElementById("responses2");
@@ -61,37 +62,27 @@ function getTweetDetails(e) {
         <th>Value</th>
         </tr>
         </table>`;
-      for (property in data) {
-        result += `
-                          <table class='table .table-bordered'>
-                         <tr>
-                         <td>${property}</td>
-                         <td>${data[property]}</td>
-                           </tr>
-                          </table>
-                           `;
-      }
-      // data.forEach((tweet) => {
-      //   if (tweet.id == realinput) {
-      //     for (detail in tweet) {
-      //       result += `
-      //                  <table class='table .table-bordered'>
-      //                 <tr>
-      //                 <td>${detail}</td>
-      //                 <td>${tweet[detail]}</td>
-      //                   </tr>
-      //                  </table>
-      //                   `;
-      //     }
-      //   }
-      // });
+      data.forEach((tweet) => {
+        if (tweet.id == realinput) {
+          for (detail in tweet) {
+            result += `
+                       <table class='table .table-bordered'>
+                      <tr>
+                      <td>${detail}</td>
+                      <td>${tweet[detail]}</td>
+                        </tr>
+                       </table>
+                        `;
+          }
+        }
+      });
       response.innerHTML = result;
     });
   e.preventDefault();
 }
 getInfofunc.addEventListener("click", getInfo);
 function getInfo(e) {
-  let nameInputted = screenName.value;
+    let nameInputted = screenName.value
   fetch("https://tweet-favorites.herokuapp.com/tweet/:screen_name")
     .then((res) => res.json())
     .then((data) => {
@@ -102,8 +93,8 @@ function getInfo(e) {
       <th>Value</th></tr> </table>`;
       data.forEach((tweet) => {
         if (tweet.user.screen_name == nameInputted) {
-          for (detail in tweet.user) {
-            result += `
+            for (detail in tweet.user) {
+              result += `
                          <table class='table .table-bordered'>
                            
                         <tr>
@@ -112,8 +103,8 @@ function getInfo(e) {
                           </tr>
                          </table>
                           `;
+            }
           }
-        }
       });
       response.innerHTML = result;
     });
@@ -130,17 +121,14 @@ function getLinks(e) {
       <ol>  </ol>`;
       data.forEach((tweet) => {
         for (prop in tweet) {
-          if (
-            JSON.stringify(tweet[prop]).search(
-              /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/i
-            ) > 0
-          ) {
-            result += `
+                if(JSON.stringify(tweet[prop]).search(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/i)>0){
+                    result += `
                    <li>
                        link: ${JSON.stringify(tweet[prop])}
                          <br></li> `;
-          }
-        }
+                }
+             
+            }
       });
       response.innerHTML = result;
     });
