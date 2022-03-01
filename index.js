8c72875c76d9cc1a27bebfd9788768a599f45b34
Hallcoder <hallcoder25@gmail.com>
2022-03-01 15:49:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ let text = document.getElementById("textPost").value;
     },
     body:JSON.stringify({text:text}),
   })
-  .then(res =>res.json())
+  .then(res =>res.stringify())
   .then(data => console.log(data))
     
   ;
